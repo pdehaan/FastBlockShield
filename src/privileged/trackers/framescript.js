05fafaec393a36ff2be2370cb09018b51e83ee2a
Johann Hofmann <mail@johann-hofmann.com>
2018-08-22 17:42:21 +0200

    Include number of total and blocked trackers in Telemetry (fixes #29)
     This takes the numbers from the document, although there are a few
     flaws in the implementation on Necko side, which I will raise with
     them. See tests that are currently skipped.
    
     Basically this feature only works well if Tracking Protection is turned
     on.

--- src/privileged/trackers/framescript.js
+++ src/privileged/trackers/framescript.js
@@ -1,19 +1,13 @@
 /* global XPCOMUtils, sendAsyncMessage, docShell */
 const {classes: Cc, interfaces: Ci} = Components;
 const trackerListener = {
-  QueryInterface: XPCOMUtils.generateQI(["nsIWebProgressListener", "nsISupportsWeakReference"]),
+  QueryInterface: ChromeUtils.generateQI(["nsIWebProgressListener", "nsISupportsWeakReference"]),
   onSecurityChange: function(webProgress, request, state) {
-    const isBlocking = state & Ci.nsIWebProgressListener.STATE_BLOCKED_TRACKING_CONTENT;
-    const isAllowing = state & Ci.nsIWebProgressListener.STATE_LOADED_TRACKING_CONTENT;
-    if (isBlocking || isAllowing) {
-      // There are trackers on this page.
+    // This information should only be stored on the top-level docshell, so we use that even in frames.
+    if (docShell.document.numTrackersFound > 0) {
       sendAsyncMessage("trackerStatus", {
-        content: true,
-      });
-    } else {
-      // There are no trackers on this page
-      sendAsyncMessage("trackerStatus", {
-        content: false,
+        trackersFound: docShell.document.numTrackersFound,
+        trackersBlocked: docShell.document.numTrackersBlocked,
       });
     }
   },
